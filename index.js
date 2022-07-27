c9d54ebf7cf766453e2d0760fbc4d648d0721e63
Victor Aguiar <victoraguiarrosa397@gmail.com>
2022-07-26 21:12:42 -0300

    just clock

--- index.js
+++ index.js
@@ -7,9 +7,16 @@ function load(){
 	
 	if (hora[0] >= 00 && hora[0] < 12){
 		adecir.innerHTML = `Good Morning!`;
+		document.body.style.backgroundColor = '#6F8FA2';
 	}else if (hora[0] > 12 && hora[0] < 18 ){
 		adecir.innerHTML = `Good Afternoon!`;
+		document.body.style.backgroundColor = '#AA8239';
 	}else if (hora[0] >= 18 && hora[0] < 24){
 		adecir.innerHTML = `Good Evening!`;
+		document.body.style.backgroundColor = '#68505E';
 	};
+	
+	setTimeout(()=>{
+		load();
+	}, 1 * 1000);
 };

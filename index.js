a950fe40222949ef2d4fd095f3cb3199128cf255
Victor Aguiar <victoraguiarrosa397@gmail.com>
2022-07-28 09:40:15 -0300

    Img

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
+let yafue = false;
+
 function load(){
 	let data = new Date();
 	let hora = [data.getHours(), data.getMinutes()];
 	let msg = document.getElementById('clock-msg');
 	let adecir = document.getElementById('adecir');
+	let clock = document.getElementById('clock');
+	let paisa = document.createElement('img');
+	paisa.setAttribute('class', 'paisagem');
 	msg.innerHTML = `Now is ${hora[0]}:${hora[1]}.`;
 	
-	if (hora[0] >= 00 && hora[0] < 12){
-		adecir.innerHTML = `Good Morning!`;
-		document.body.style.backgroundColor = '#6F8FA2';
-	}else if (hora[0] > 12 && hora[0] < 18 ){
-		adecir.innerHTML = `Good Afternoon!`;
-		document.body.style.backgroundColor = '#AA8239';
-	}else if (hora[0] >= 18 && hora[0] < 24){
-		adecir.innerHTML = `Good Evening!`;
-		document.body.style.backgroundColor = '#68505E';
+	if (yafue == false){
+		if (hora[0] >= 00 && hora[0] < 12){
+			document.body.style.backgroundColor = '#6F8FA2';
+			paisa.src = 'img/manha.jpg';
+			clock.appendChild(paisa);
+			adecir.innerHTML = `Good Morning! (◠▽◠)`;
+		}else if (hora[0] > 12 && hora[0] < 18 ){
+			document.body.style.backgroundColor = '#AA8239';
+			paisa.src = 'img/tarde.jpg';
+			clock.appendChild(paisa);
+			adecir.innerHTML = `Good Afternoon! (o･ω･o)`;
+		}else if (hora[0] >= 18 && hora[0] < 24){
+			document.body.style.backgroundColor = '#68505E';
+			paisa.src = 'img/noite.jpg';
+			clock.appendChild(paisa);
+			adecir.innerHTML = `Good Evening! ＼(￣▽￣)／`;
+		};
+		yafue = true;
 	};
 	
 	setTimeout(()=>{
